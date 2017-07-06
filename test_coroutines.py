5e16996b8e7cfc73cc542f873b0eb4be65bfef9c
jakobpederson <jakob.pederson+github@gmail.com>
2017-07-06 11:16:01 -0500

    add test

--- test_coroutines.py
+++ test_coroutines.py
@@ -23,13 +23,17 @@ class CoroutinesTest(unittest.TestCase):
         self.assertEqual([1, 2, 3, 4, 5], result)
 
     def test_complex_print(self):
-        test_co = coroutines.complex_print(2)
-        test_co_5 = coroutines.complex_print(5)
-        test_co_5.send(2)
-        test_co.send(9)
-        test_co_5.send(3)
-        self.assertEqual()
-        self.fail('x')
+        from contextlib import redirect_stdout
+        from io import StringIO
+        out = StringIO()
+        with redirect_stdout(out):
+            test_co = coroutines.complex_print(2)
+            test_co_5 = coroutines.complex_print(5)
+            test_co_5.send(2)
+            test_co.send(9)
+            test_co_5.send(3)
+        result = out.getvalue().strip().split('\n')
+        self.assertEqual(['10', '18', '15'], result)
 
     def test_super_complicated_example(self):
         test_co = coroutines.super_complicated_example()
